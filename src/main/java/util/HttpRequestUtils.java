89653ba080922f19579202616e2344df3ed62837
MJbae <gently.gogo@gmail.com>
2021-03-30 12:30:55 +0900

    Feat: index.html 요청에 대한 응답 전송 기능 구현
    1. 요청에서 header 정보 중 uri 부분 추출
    2. 서버에서 해당 파일 찾아서 byte로 읽기
    3. byte로 읽은 부분을 body로 감싸서 전송

--- src/main/java/util/HttpRequestUtils.java
+++ src/main/java/util/HttpRequestUtils.java
@@ -6,11 +6,21 @@ import java.util.stream.Collectors;
 
 import com.google.common.base.Strings;
 import com.google.common.collect.Maps;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import webserver.RequestHandler;
 
 public class HttpRequestUtils {
+    private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
+
+    public static String getUrl(String line) {
+        String[] splited = line.split(" ");
+        log.debug("request path: {}", splited[1]);
+        return splited[1];
+    }
+
     /**
-     * @param queryString은
-     *            URL에서 ? 이후에 전달되는 field1=value1&field2=value2 형식임
+     * @param queryString은 URL에서 ? 이후에 전달되는 field1=value1&field2=value2 형식임
      * @return
      */
     public static Map<String, String> parseQueryString(String queryString) {
@@ -18,8 +28,7 @@ public class HttpRequestUtils {
     }
 
     /**
-     * @param 쿠키
-     *            값은 name1=value1; name2=value2 형식임
+     * @param 쿠키 값은 name1=value1; name2=value2 형식임
      * @return
      */
     public static Map<String, String> parseCookies(String cookies) {
